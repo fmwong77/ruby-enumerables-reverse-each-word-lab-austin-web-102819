038b6aff664fc81cb494dcc9f933263109ed6824
Mei Wong <fongmeiw@gmail.com>
2019-09-25 20:25:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(str)
   splitStr = str.split("")
   reversed = []
   
-  str.size.times {reversed << splitStr.pop}
+  str.size.times {reversed << splitStr.unshift}
   reversed.join
 end
 
