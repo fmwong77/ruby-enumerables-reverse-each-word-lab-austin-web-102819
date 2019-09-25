d51e753b9d77f71a5c96932267bec43cb740883f
Mei Wong <fongmeiw@gmail.com>
2019-09-25 20:35:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(str)
   arrStr = %w(str)
   reversed = []
   puts arrStr
-  str.size.times {reversed << arrStr.pop}
+  arrStr.each {reversed << arrStr.pop}
   reversed.join
 end
 
